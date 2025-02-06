a914e23392c5def8a753caf68f4fa2161b886036
nafis195 <nafisbuet1@gmail.com>
2025-02-05 19:27:13 -0800

    adding utils file

---
+++ utils.py
@@ -0,0 +1,12 @@
+import time
+from pprint import pprint
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.firefox.service import Service
+# from selenium.webdriver.firefox.options import Options
