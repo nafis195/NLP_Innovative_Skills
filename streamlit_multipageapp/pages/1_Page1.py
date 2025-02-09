d85e7c2b623ea0aa2f727fdee12707182d1f87d1
nafis195 <nafisbuet1@gmail.com>
2025-02-08 20:07:41 -0800

    streamlit multi pages app

---
+++ streamlit_multipageapp/pages/1_Page1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="First Page")
+st.title("Page 1")
+st.text_input("Name")
---
+++ streamlit_multipageapp/pages/2_Page2.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Second Page", page_icon="📈")
+st.title("Page 2")
